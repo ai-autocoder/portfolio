c1943ab6767ef9844cbf82276f47255e6f036e96
Francesco Anzalone <francesco.a@mail.com>
2023-11-19 22:14:37 +0000

    Add Italian language support using i18next and implement a language switcher.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-(function menuListener() {
-  const openButton = document.getElementById("open-menu-btn");
-  const closeButton = document.getElementById("close-menu-btn");
-  const navElem = document.getElementById("nav");
-  openButton.addEventListener("click", (e) => {
-    navElem.classList.add("show-menu");
-  });
-  closeButton.addEventListener("click", () => {
-    navElem.classList.remove("show-menu");
-  });
-})();
+import { initializeI18next, updateContent } from "./locales/language.js";
+import i18next from "i18next";
+
+initializeI18next().then(() => {
+	updateContent();
+});
+
+i18next.on("languageChanged", () => {
+	updateContent();
+});
+
+// Language switch
+
+document.getElementById("lang-switch").checked = i18next.language === "en";
+document.getElementById("lang-switch").addEventListener("click", () => {
+	const currentLanguage = i18next.language;
+	const newLanguage = currentLanguage === "en" ? "it" : "en";
+	i18next.changeLanguage(newLanguage);
+});
+
+// Nav open / close
+const navElem = document.getElementById("nav");
+document.getElementById("open-menu-btn").addEventListener("click", (e) => {
+	navElem.classList.add("show-menu");
+});
+document.getElementById("close-menu-btn").addEventListener("click", () => {
+	navElem.classList.remove("show-menu");
+});
